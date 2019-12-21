d64ff2d4349c0b5d544489cb12b8ef2416ba6798
MoovSpace <57452238+moovspace@users.noreply.github.com>
2019-12-21 16:58:56 +0100

    Php upload file script
    Php upload file script

--- upload.php
+++ upload.php
@@ -6,4 +6,37 @@ print_r($_POST);
 
 // Show files array
 print_r($_FILES);
+
+// Allow files with extension
+$allow = ["jpg", "png", "gif", "zip", "pdf"];
+
+// Upload
+if(!empty($_FILES["file"]["tmp_name"]))
+{
+  // File extension
+  $ext = strtolower(pathinfo($_FILES["file"]["name"], PATHINFO_EXTENSION));
+  
+  // Test extension
+  if(in_array($ext, $allow))
+  { 
+    // Upload dir
+    $dir = "media/files";
+    
+    // Create dir
+    mkdir($dir, 0777, true);
+    chmod($dir,0777);
+    
+    // New file name
+    $path = $dir . "/" . md5(microtime().uniqid()) . "." . $ext; 
+    
+    // Save file
+    $ok = move_uploaded_file($_FILES["file"]["tmp_name"], $path);
+
+  }else{
+    echo "Error extension";
+  }
+  
+}else{
+  echo "Empty file";
+}
 ?>
