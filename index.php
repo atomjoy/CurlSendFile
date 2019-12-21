f67024b317636985e6a47d72bc210b19b14fb1a6
MoovSpace <57452238+moovspace@users.noreply.github.com>
2019-12-21 16:07:15 +0100

    Send file with curl from php
    Send file and post data with curl from php.

--- index.php
+++ index.php
@@ -6,7 +6,13 @@ try
 {
     $curl = new CurlSendFile();
     
-    echo $curl->Send("https://localhost:4443/upload.php", 'img/wolf.jpg', ['id'=>'1234567890']);    
+    // Send single file
+    echo $curl->Send("http://localhost/upload.php", 'img/wolf.jpg', ['id'=>'1234567890']);
+        
+    $timeout = 60; // Timeout default: 60
+    $selfsigned = 1; // Allow selfsigned ssl 1 or 0 default: 1
+    $attr = 'my_file'; // Files form name $_FILES['my_file'] default: file
+    echo $curl->Send("https://localhost/upload.php", 'img/wolf.jpg', ['id'=>'1234567890'], $timeout, $selfsigned, $attr);
 }
 catch(Exception $e)
 {
