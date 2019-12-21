88f670000fe218aa141cc7e3fe962dd48fc02551
MoovSpace <57452238+moovspace@users.noreply.github.com>
2019-12-21 15:58:26 +0100

    Send file with curl from php
    Send file and post data with curl from php

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
+// Import class
+require('CurlSendFile.php');
+
 try
 {
     $curl = new CurlSendFile();
+    
     echo $curl->Send("https://localhost:4443/upload.php", 'img/wolf.jpg', ['id'=>'1234567890']);    
 }
 catch(Exception $e)
