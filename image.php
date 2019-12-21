2df9537aeefdf4c68e9df1fab9795d4a38ea78a3
Marcys <moovspace@github.com>
2019-12-21 18:59:17 +0100

    Show image secure

--- image.php
+++ image.php
@@ -12,7 +12,7 @@ if(!empty($_GET['file']) && file_exists($_GET['file'])){
     }
 }
 // Error file
-$f = "img/wolf.jpg";
+$f = "img/error-file.png";
 header('Content-Type: ' . mime_content_type($f));
 header("Content-Length: " . filesize($f));
 readfile($f);
