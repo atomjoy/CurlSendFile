8f9348e4ddbcd453c035fdf99e91d8c421742cbf
MoovSpace <57452238+moovspace@users.noreply.github.com>
2019-12-21 15:54:56 +0100

    Send file with curl
    Send file with curl

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+try
+{
+    $curl = new CurlSendFile();
+    echo $curl->Send("https://localhost:4443/upload.php", 'img/wolf.jpg', ['id'=>'1234567890']);    
+}
+catch(Exception $e)
+{
+    echo $e->getMessage();
+    echo $e->getCode();
+}
+?>
