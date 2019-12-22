1b6dde962f4fc872b22c0136142a6dedbed1496d
Marcys <moovspace@github.com>
2019-12-22 14:00:08 +0100

    Upload big files with curl and php

--- CurlSendFile.php
+++ CurlSendFile.php
@@ -16,7 +16,7 @@ class CurlSendFile
      * @param string $form_attr Files array $_FILES['file'] field name: file
      * @return string Json string array
      */
-    function Send($url, $f, $data = [], $timeout = 60, $selfsigned = 1, $form_attr = 'file', $token = ""){
+    function Send($url, $f, $data = [], $timeout = 60, $selfsigned = 1, $form_attr = 'file', $token = "", $user = "", $pass = ""){
         $f = realpath($f);
         // $_POST
         foreach ($data as $k => $v) { $post[$k] = $v; }
@@ -37,6 +37,9 @@ class CurlSendFile
             curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
             curl_setopt($ch, CURLOPT_SSLVERSION, 6);
+        }
+        if(!empty($user) && !empty($pass)){
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Basic ".base64_encode($user.':'.$pass)));
         }
         if(!empty($token)){
             curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer ".$token));
