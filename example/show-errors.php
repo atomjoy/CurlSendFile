5e8e2bee5e2b703675958631b2029e00d0246645
Marcys <moovspace@github.com>
2019-12-23 16:52:54 +0100

    Php show errors

--- example/show-errors.php
+++ example/show-errors.php
@@ -1,6 +1,9 @@
 <?php
+// Limits
 set_time_limit(0);
 ini_set('memory_limit', -1); // -1 unlimited
+
+// Errors
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
