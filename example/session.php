86481b4d4c5aad8018d8fe2c0cf666dd9e46b120
Marcys <moovspace@github.com>
2019-12-23 16:54:54 +0100

    Php secure session

--- example/session.php
+++ example/session.php
@@ -1,6 +1,10 @@
 <?php
 // Secure session
-session_set_cookie_params(0, '/', '.' . $_SERVER['SERVER_NAME'], isset($_SERVER['HTTPS']), true);
+// session_set_cookie_params(0, '/', '.' . $_SERVER['SERVER_NAME'], isset($_SERVER['HTTPS']), true);
+
+// Import classess from composer here
+// require('vendor\autoload.php');
+
 // Start session
 session_start();
 ?>
